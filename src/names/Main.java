d29806ab9ff32e4b5164259cfb988ecad50fb427
Yasser <ye9@duke.edu>
2020-08-25 02:24:41 -0400

    Initialized try/catch in Main

--- src/names/Main.java
+++ src/names/Main.java
@@ -1,14 +1,33 @@
 package names;
 
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
-public class Main {
+public class Main
+{
+    public static final String DATA_FILE_TEST = "yob1900.txt";
     /**
      * Start of the program.
      */
-    public static void main (String[] args) {
-        System.out.println("Hello world");
+    public static void main (String[] args)
+    {
+        try
+        {
+            Path path = Paths.get(Main.class.getClassLoader().getResource(DATA_FILE_TEST).toURI());
+            for (String line: Files.readAllLines(path))
+            {
+
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
