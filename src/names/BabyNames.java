7c99b03bb33dc4913a6afbb739963cb368b617fa
Yasser <ye9@duke.edu>
2020-08-25 21:16:18 -0400

    Initialized BabyNames class

--- src/names/BabyNames.java
+++ src/names/BabyNames.java
@@ -1,18 +1,24 @@
 package names;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
 public class BabyNames
 {
-    private int year;
+    private List<Integer> years;
+    private List<YearOfBirthFile> data;
 
-    public static void main (String[] args)
+    public BabyNames(List<Integer> years)
     {
+        this.years = years;
+        data = new ArrayList<>();
 
+        for (int year: years)
+        {
+            data.add(new YearOfBirthFile(year));
+        }
     }
-
-    public
-
-
 }
