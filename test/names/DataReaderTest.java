adb66348c7e0814d8289e6ffb136f954930d7b99
Yasser <ye9@duke.edu>
2020-09-01 05:28:48 -0400

    Updated readDataSet test:

--- test/names/DataReaderTest.java
+++ test/names/DataReaderTest.java
@@ -9,15 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class DataReaderTest {
 
     @Test
-    void readDataSet() throws Exception {
+    void readDataSet() throws Exception
+    {
         DataReader testReading = new DataReader("read_test");
-        List<Integer> expectedYears = List.of(2000);
+        List<Integer> expectedYears = List.of(1111, 1112, 1113, 1114);
         List<Integer> actualYears = testReading.getYears();
         assertEquals(expectedYears, actualYears);
     }
-
-
-
-
-
 }
